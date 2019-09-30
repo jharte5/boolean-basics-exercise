c47714b3a94a268cfd8c64d8f2b634b75193dab6
jharte5 <jonathan.harte@codeimmersives.com>
2019-09-30 14:38:17 -0400

    finally figured out the rest

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 let iGetTheJoke = true;
 let havingFun = true;
 let learning = true;
-let killingIt = true;
+let killingIt = learning && havingFun;
 
 
-function returnFalse(condition) {
-learning && havingFun == killingIt;
+function returnFalse() {
+// learning && havingFun == killingIt;
+return false;
+ 
 }
 
 function isOpposite(condition) {
-
-  !iGetTheJoke;
+  return !condition;
+  // return !killingIt;
 }
 
 function both(condition1, condition2) {
-
+  // return true || false;
+  // return if !condition1;
+  return condition1 && condition2;
+  
 }
 
 function either(condition1, condition2) {
+  return condition1 || condition2;
 
 }
 
 function neither(condition1, condition2) {
+  return !condition1 && !condition2  
 
 }
 
 function itsComplicated(condition1, condition2, condition3) {
+  return !condition1 || condition2 && condition3
 
 }
 
